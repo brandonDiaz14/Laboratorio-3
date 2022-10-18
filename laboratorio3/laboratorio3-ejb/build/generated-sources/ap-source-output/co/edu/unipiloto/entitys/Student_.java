48eb439230991ccb083a1e44869931617541a6ff
brandonDiaz14 <bsdiaz60@misena.edu.co>
2022-10-18 00:53:00 -0500

    se agrego laboratorio 3

---
+++ laboratorio3/laboratorio3-ejb/build/generated-sources/ap-source-output/co/edu/unipiloto/entitys/Student_.java
@@ -0,0 +1,16 @@
+package co.edu.unipiloto.entitys;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-18T00:49:17")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, String> firstname;
+    public static volatile SingularAttribute<Student, Integer> yearlevel;
+    public static volatile SingularAttribute<Student, Integer> studenId;
+    public static volatile SingularAttribute<Student, String> lastname;
+
+}
